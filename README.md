cdbdd9cda8fd557d6f746f76695a935c8078f813
Vladimir Rodkin <mail@vovanr.com>
2017-03-26 13:29:23 +0300

    Init

---
+++ README.md
@@ -0,0 +1,29 @@
+# test-fotostrana
+
+> Тестовое задание для Фотостраны
+
+- [PostCSS](https://github.com/postcss/postcss)
+- [PreCSS](https://github.com/jonathantneal/precss)
+- [Autoprefixer](https://github.com/postcss/autoprefixer)
+- [CSSO](https://github.com/css/csso)
+- [stylelint](https://github.com/stylelint/stylelint) with [standard config](https://github.com/stylelint/stylelint-config-standard)
+- [postcss-style-guide](https://github.com/morishitter/postcss-style-guide)
+
+## Install
+```
+npm i
+```
+
+## Build
+```
+npm run build
+```
+
+## Develop
+```
+npm start
+```
+Open [localhost:8080/webpack-dev-server](http://localhost:8080/webpack-dev-server/)
+
+## License
+MIT © [Vladimir Rodkin](https://github.com/VovanR)
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+const ENV = {
+  production: 'production',
+  development: 'development'
+};
+
+const isDev = process.env.NODE_ENV !== ENV.production;
+
+module.exports = {
+  entry: {
+    app: './src/js/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/assets/',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: false,
+                url: false,
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                // sourceMap: 'inline',
+                plugins: function () {
+                  const plugins = [
+                    require('precss')(),
+                    require('postcss-flexbugs-fixes')(),
+                    require('postcss-clearfix')(),
+                    require('autoprefixer')({cascade: false})
+                  ];
+
+                  if (isDev) {
+                    return plugins;
+                  }
+
+                  plugins.push(
+                    require('postcss-csso')()
+                  );
+                  return plugins;
+                }
+              }
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: (() => {
+    const plugins = [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(isDev ? ENV.development : ENV.production)
+      }),
+      new StyleLintPlugin({
+        files: './src/**/*.css'
+      }),
+      new ExtractTextPlugin({
+        filename: '[name].css',
+        allChunks: true
+      })
+    ];
+
+    return plugins;
+  })(),
+  devtool: isDev && 'cheap-module-source-map'
+};
